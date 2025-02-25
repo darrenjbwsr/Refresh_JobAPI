2754a9ad402a98ab26c4ee9fa363ccfbd23cceb9
Darren Bowser <darrenjbwsr@gmail.com>
2025-02-24 21:33:17 -0500

    Loggout function

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -52,9 +52,25 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
             process.env.JWT_SECRET || "default",
             {expiresIn: '12hr'}
         )
-        res.status(200).json({token})
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 12 * 60 * 60 * 1000
+        })
+        res.status(200).json({ message : "Login successful"})
     } catch (err){
         console.error(err);
         res.status(500).json({message:"server error"})
+    }
+}
+
+export const logoutUser = async (req: Request, res: Response): Promise<void> => {
+    try {
+        res.clearCookie("token");
+        res.status(200).json({ message: "User logged out successfully"})
+    } catch (error) {
+        console.error("Logout error", error);
+        res.status(500).json({ message: "server error"})
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,17 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import {PrismaClient} from "@prisma/client";
-import authRoutes from "./routes/authRoutes"
+import authRoutes from "./routes/authRoutes";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors());
-app.use(express.json())
-app.use('/auth', authRoutes)
+app.use(cookieParser());
+app.use(express.json());
+app.use('/auth', authRoutes);
 
 app.get("/", (req, res) => {
     res.send("Refresh is running")
